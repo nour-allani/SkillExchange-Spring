c5baaf55061a5e724df5bf37e4a72a455468cac2
MRGHOSJ <bouzouitayassine@gmail.com>
2025-03-08 12:14:37 +0100

    1.2.0
    added web config to allow corsmapping

--- src/main/java/tn/esprit/skillexchange/Controller/GestionUser/AuthentificationController.java
+++ src/main/java/tn/esprit/skillexchange/Controller/GestionUser/AuthentificationController.java
@@ -2,10 +2,7 @@ package tn.esprit.skillexchange.Controller.GestionUser;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esprit.skillexchange.Entity.GestionUser.Authentication.RefreshTokenRequest;
 import tn.esprit.skillexchange.Entity.GestionUser.Authentication.SignInRequest;
 import tn.esprit.skillexchange.Entity.GestionUser.Authentication.SignUpRequest;
