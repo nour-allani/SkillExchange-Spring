afc1acbb743f90146edc1a8cb80c1733dddc4faa
onsgharbii <ons.gharbi@esprit.tn>
2025-04-08 09:17:17 +0100

    add-post

--- src/main/java/tn/esprit/skillexchange/Controller/GestionProduit/ReviewProductController.java
+++ src/main/java/tn/esprit/skillexchange/Controller/GestionProduit/ReviewProductController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.skillexchange.Entity.GestionProduit.DTO.ReviewProductDTO;
+
 import tn.esprit.skillexchange.Entity.GestionProduit.ReviewProduct;
 import tn.esprit.skillexchange.Repository.GestionUser.UserRepo;
 import tn.esprit.skillexchange.Service.GestionProduit.IReviewProductService;
--- src/main/java/tn/esprit/skillexchange/Repository/GestionProduit/ReviewProductRepo.java
+++ src/main/java/tn/esprit/skillexchange/Repository/GestionProduit/ReviewProductRepo.java
@@ -3,6 +3,10 @@ package tn.esprit.skillexchange.Repository.GestionProduit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.skillexchange.Entity.GestionProduit.ReviewProduct;
+
+import java.util.List;
+
 @Repository
 public interface ReviewProductRepo extends JpaRepository<ReviewProduct,Long> {
+    List<ReviewProduct> findByProduct_IdProduct(Long productId);
 }
